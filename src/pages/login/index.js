e14ec79c5345866f2e472adf224e03f3c0d9cbb5
João Pedro Sousa <jassadosgame@live.com>
2021-07-27 11:37:55 -0300

    detals frontend

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './login.css'
-import { FaBeer } from 'react-icons/fa'
+import { MdMail } from "react-icons/md";
+import { MdLock } from "react-icons/md";
 
 const login = () => {
     return (
@@ -11,16 +12,22 @@ const login = () => {
            </div>
               <div className="login-right">
                   <h1>Acessar app</h1>
-                  <div className="loginInputEmail">
+                  <div className="loginEmail">
+                      <MdMail />
                       <input type="text"
                       placeholder="Digite um email">
                       </input>
                   </div>
                   <div className="loginPassword">
+                      <MdLock />
                       <input type="text"
                       placeholder="Digite sua senha">
                       </input>
-                  </div>
+                  </div>   
+                      <button type="submit">Entrar</button>
+                      <h4>Nao tenho conta</h4>
+                      <button type="submit">Cadastro</button>
+                  
               </div> 
            
 
@@ -30,3 +37,5 @@ const login = () => {
 }
 
 export default login;
+                      
+                      
